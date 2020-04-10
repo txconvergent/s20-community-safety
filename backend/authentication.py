30ed5935cf519f592e987a350bfc4d5a8b820fbb
richard.ai <52604283+RichardMathewsII@users.noreply.github.com>
2020-04-10 18:36:07 -0500

    Update authentication.py

--- backend/authentication.py
+++ backend/authentication.py
@@ -9,16 +9,40 @@ config = {
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 
-invalid = True
-while invalid:
-  # connect data pipeline to frontend for email and password
-  email = input("Enter University Email Address: ")
-  password = input("Enter Password: ")
+# connect button from frontend authentication page
+button = 'Create Account'
+# or
+# button = 'Sign In'
+# or
+# button = 'Reset Password'
 
-  domain = email[-10:]
-  if domain == 'utexas.edu':
-    invalid = False
-    user = auth.create_user_with_email_and_password(email, password)
-  else:
-    print('Please enter email address with utexas.edu domain')
+# connect data pipeline to frontend for email and password
+email = input("Enter University Email Address: ")
+password = input("Enter Password: ")
 
+if button = 'Create Account':
+  invalid = True
+  while invalid:
+
+    domain = email[-10:]
+    if domain == 'utexas.edu':
+      invalid = False
+      user = auth.create_user_with_email_and_password(email, password)
+      signin = auth.sign_in_with_email_and_password(email, password)
+      auth.send_email_verification(signin['idToken'])
+      print('Email verification has been sent')
+    else:
+      print('Please enter email address with utexas.edu domain')
+
+if button = 'Sign In':
+  invalid = True
+  while invalid:
+    try:
+      signin = auth.sign_in_with_email_and_password(email, password)
+      invalid = False
+    except:
+      print('Invalid email or password')
+
+if button = 'Reset Password':
+  auth.send_password_reset_email(email)
+  print('We sent you an email to reset your password')
