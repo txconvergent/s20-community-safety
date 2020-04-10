b8455fb44ff4425d15c94754780aa7d3aa40606c
richard.ai <52604283+RichardMathewsII@users.noreply.github.com>
2020-04-10 18:56:57 -0500

    Update crime_data.py

--- backend/crime_data.py
+++ backend/crime_data.py
@@ -1,1 +1,11 @@
+import os
 import pandas as pd
+import numpy as np
+from sodapy import Socrata
+
+dataset_id = 'fdj4-gpfu'
+domain = 'data.austintexas.gov'
+client = Socrata(domain, None)
+
+results = client.get(dataset_id)
+df = pd.DataFrame.from_dict(results)
