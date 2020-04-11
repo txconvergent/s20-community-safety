bbda544f93aa47a83a750535b263db6402cbb929
richard.ai <52604283+RichardMathewsII@users.noreply.github.com>
2020-04-11 16:24:52 -0500

    Update crime_data.py

--- backend/crime_data.py
+++ backend/crime_data.py
@@ -9,3 +9,10 @@ client = Socrata(domain, None)
 
 results = client.get(dataset_id)
 df = pd.DataFrame.from_dict(results)
+
+# UT austin region: latitude and longitude
+bottom_left = (30.282200, -97.754840)
+bottom_right = (30.287402, -97.754840)
+top_left = (30.282200, -97.748456)
+top_right = (30.287402, -97.748456)
+
