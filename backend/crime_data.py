c5365c8910657c81348f06877075f6b21480dbc0
richard.ai <52604283+RichardMathewsII@users.noreply.github.com>
2020-04-13 13:35:31 -0500

    Update crime_data.py

--- backend/crime_data.py
+++ backend/crime_data.py
@@ -11,8 +11,14 @@ results = client.get(dataset_id)
 df = pd.DataFrame.from_dict(results)
 
 # UT austin region: latitude and longitude
-bottom_left = (30.282200, -97.754840)
-bottom_right = (30.287402, -97.754840)
-top_left = (30.282200, -97.748456)
-top_right = (30.287402, -97.748456)
+top_left = (30.287402, -97.754840)
+bottom_right = (30.282200, -97.748456)
+UTregion = [top_left, bottom_right]
 
+class CrimeData():
+  
+  def __init__(self, region):
+    self.top = region[0][0]
+    self.bottom = region[1][0]
+    self.left = region[0][1]
+    self.right = region[1][1]
