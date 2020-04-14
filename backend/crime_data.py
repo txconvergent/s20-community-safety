366bdb28b1f669d7c8f2c7fe04596e00ac007c88
richard.ai <52604283+RichardMathewsII@users.noreply.github.com>
2020-04-14 17:39:48 -0500

    Update crime_data.py

--- backend/crime_data.py
+++ backend/crime_data.py
@@ -12,9 +12,11 @@ class CrimeData():
     self.bottom = region[1][0]
     self.left = region[0][1]
     self.right = region[1][1]
+    self.df = None
+    self.client = None
     
   def load(self, dataset_id, domain, socrata_token, start_year, max_crimes):
-    client = Socrata(domain, socrata_token)
+    self.client = Socrata(domain, socrata_token)
     incident_id = start_year * (10**7)
     query = "incident_report_number > " + str(incident_id) + \
             " and incident_report_number < 20210000000 " + \
@@ -22,12 +24,12 @@ class CrimeData():
             " and latitude < " + str(self.top) + \
             " and longitude > " + str(self.left) + \
             " and longitude < " + str(self.right)
-    dataset = client.get(dataset_id, where = query, limit = max_crimes)
-    df = pd.DataFrame.from_dict(dataset)
+    dataset = self.client.get(dataset_id, where = query, limit = max_crimes)
+    self.df = pd.DataFrame.from_dict(dataset)
   
   def crime_count(self):
     try:
-      return df[incident_report_number].count()
+      return self.df[incident_report_number].count()
     except:
       print('No dataset has been loaded into this instance')
   
@@ -36,8 +38,8 @@ class CrimeData():
     center_lng = self.right + (self.left - self.right) / 2
     gmap = gmplot.GoogleMapPlotter(center_lat, center_lng, 17)
     try:
-      lat_coordinates = df['latitude'].astype('float').tolist()
-      lng_coordinates = df['longitude'].astype('float').tolist()
+      lat_coordinates = self.df['latitude'].astype('float').tolist()
+      lng_coordinates = self.df['longitude'].astype('float').tolist()
       gmap.scatter(lat_coordinates, lng_coordinates, '#FF0000', size=5, marker=False)
       return gmap
     except:
@@ -48,15 +50,15 @@ class CrimeData():
     center_lng = self.right + (self.left - self.right) / 2
     gmap = gmplot.GoogleMapPlotter(center_lat, center_lng, 17)
     try:
-      lat_coordinates = df['latitude'].astype('float').tolist()
-      lng_coordinates = df['longitude'].astype('float').tolist()
+      lat_coordinates = self.df['latitude'].astype('float').tolist()
+      lng_coordinates = self.df['longitude'].astype('float').tolist()
       gmap.heatmap(lat_coordinates, lng_coordinates, radius=30)
       return gmap
     except:
       print('No dataset has been loaded into this instance')
   
   def close(self):
-    client.close()
+    self.client.close()
   
   def show_bottom(self):
     print(self.bottom)
