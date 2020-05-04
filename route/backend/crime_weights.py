c88e998d59be59dca46633c9c325299641a7699e
richard.ai <52604283+RichardMathewsII@users.noreply.github.com>
2020-05-03 21:31:44 -0500

    Update crime_weights.py

--- route/backend/crime_weights.py
+++ route/backend/crime_weights.py
@@ -1,1 +1,8 @@
 #
+def generate_crime_weights(crime_dataset):
+  """
+  :param crime_dataset: Austin crime data - type: pandas dataframe
+  :return: weights on a scale of 0 to 100 for each type of crime - type: dictionary
+  """
+  
+  
