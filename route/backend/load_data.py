b34c87860d9d9f6fe86ee8a9015dafc2812b7153
RichardMathewsII <52604283+RichardMathewsII@users.noreply.github.com>
2020-05-10 14:54:27 -0500

    Create load_data.py

---
+++ route/backend/load_data.py
@@ -0,0 +1,18 @@
+from crime_data import *
+
+
+def load_data():
+    socrata_token = 'xty4xwyMTY7JsNGnat7Av9sQt'
+
+    dataset_id = 'fdj4-gpfu'
+    domain = 'data.austintexas.gov'
+
+    # UT austin region: latitude and longitude
+    top_left = (30.299402, -97.754840)
+    bottom_right = (30.282200, -97.728456)
+    UTregion = [top_left, bottom_right]
+
+    api_key = 'AIzaSyDmKbjLrlWQowWVzzTy_AAWsFQO4Hdbeko'
+    utcrime = CrimeData(UTregion, api_key)
+    utcrime.load(dataset_id, domain, socrata_token, 2016, 2030, 10000)
+    return utcrime
